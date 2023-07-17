db2279dec367540b5e7c3aabd65ef83bf33068ae
Jonty Knox <jonty@openline.ai>
2023-07-17 20:29:01 +0100

    Add Segment test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { AnalyticsBrowser } from '@segment/analytics-next';
+
+export const analytics = AnalyticsBrowser.load({ writeKey: 'DVSutAt6T6FoA2uCzdK1OLzhQMgIXuu2' })
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={() => alert('Hello World')}>
-          Click me
+        <button onClick={() => analytics.identify('JohnsID', {
+          name: 'John Doe',
+          email: 'test@email.com'
+        })}>
+          Set user ID
+        </button>
+        <button onClick={() => analytics.track('Hello World')}>
+          Track with Segment
         </button>
       </header>
     </div>
